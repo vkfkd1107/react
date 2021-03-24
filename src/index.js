b998b70cef3bb38e6d75ee1f48a5c53cc9b716a6
jisu <ji_su1107@likelion.org>
2021-03-24 13:44:29 +0900

    props

--- src/index.js
+++ src/index.js
@@ -6,22 +6,59 @@ import reportWebVitals from './reportWebVitals';
 import { render } from '@testing-library/react';
 
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
+function formatDate(date) {
+  return date.toLocaleDateString();
 }
 
-function Name() {
+function Comment(props) {
   return (
-    <div>
-      <Welcome name="Sara" />
-      <Welcome name="Jisu" />
-      <Welcome name="Kate" />
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+function Avatar(props) {
+  return (
+    <img className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
+  )
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
     </div>
   )
 }
 
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'https://placekitten.com/g/64/64',
+  },
+};
+
 ReactDOM.render(
-  <Name />,
+  <Comment
+    date={comment.date}
+    text={comment.text}
+    author={comment.author}
+  />,
   document.getElementById('root')
 )
 
